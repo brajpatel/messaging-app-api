c1803a4791b866f702f3bcc36eff872f82761fa1
brajpatel <brajeshpatel0002@gmail.com>
2023-11-04 10:40:46 +0000

    Change image type to data

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true},
     status_message: { type: String, default: '' },
-    profile_picture: { type: Buffer, contentType: String },
+    profile_picture: { data: Buffer, contentType: String },
     password: { type: String, required: true },
     date_joined: { type: Date, required: true },
     friends: [{ type: Schema.types.ObjectId, ref: 'User', required: true }]
