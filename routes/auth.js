df81e4adab166507ebb54d757b4425df07dcf68d
brajpatel <brajeshpatel0002@gmail.com>
2023-11-04 11:20:22 +0000

    Create simple post routes to json a message

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/sign-up', function(req, res, next) {
+router.post('/sign-up', function(req, res, next) {
     res.json({ message: 'sign user up' })
 });
 
-router.get('/login', function(req, res, next) {
+router.post('/login', function(req, res, next) {
     res.json({ message: 'log user in' })
 });
 
-router.get('/logout', function(req, res, next) {
+router.post('/logout', function(req, res, next) {
     res.json({ message: 'log user out' })
 });
 
