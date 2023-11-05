7141ff93a59db262c69134f381c1d89cd723750a
brajpatel <brajeshpatel0002@gmail.com>
2023-11-05 17:54:25 +0000

    Create route for deleting user profile

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/create', function(req, res, next) {
-    res.json({ message: 'sign user up' })
+    res.json({ message: 'sign user up' });
 });
 
 router.post('/update', function(req, res, next) {
-    res.json({ message: 'update user profile' })
+    res.json({ message: 'update user profile' });
 });
+
+router.post('/deelte', function(req, res, next) {
+    res.json({ message: 'delete user profile' });
+})
 
 module.exports = router;
