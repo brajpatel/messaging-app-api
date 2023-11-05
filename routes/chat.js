96c58d4b7a0e28208696dd78eaa44b101545d1a1
brajpatel <brajeshpatel0002@gmail.com>
2023-11-05 17:39:30 +0000

    Create router for sending messages

--- routes/chat.js
+++ routes/chat.js
@@ -9,4 +9,8 @@ router.post('/delete', function(req, res, next) {
     res.json({ message: 'delete chat' })
 });
 
+router.post('/send-message', function(req, res, next) {
+    res.json({ message: 'send message' })
+})
+
 module.exports = router;
