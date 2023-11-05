2c2ad005c7c663aff88f4e668cc3cdc864f37878
brajpatel <brajeshpatel0002@gmail.com>
2023-11-05 17:53:24 +0000

    Create controllers for updating and deleting accounts

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,14 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/user');
 
 exports.create_account = asyncHandler(async (req, res, next) => {
-    
+
+})
+
+exports.update_account = asyncHandler(async (req, res, next) => {
+
+})
+
+exports.delete_account = asyncHandler(async (req, res, next) => {
+
 })
