1538c6f449ae3a377d5cdc8364463ee6559a9571
brajpatel <brajeshpatel0002@gmail.com>
2023-11-06 16:42:01 +0000

    Add minimum length requirements to username and password

--- models/user.js
+++ models/user.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: String, required: true },
+    username: { type: String, minLength: 3, required: true },
     email: { type: String, required: true},
     status_message: { type: String, default: '' },
     profile_picture: { data: Buffer, contentType: String },
-    password: { type: String, required: true },
+    password: { type: String, minLength: 6, required: true },
     date_joined: { type: Date, required: true },
     friends: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }]
 })
