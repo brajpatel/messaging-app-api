d9ec641b46398eb7d459d8b92eefea67c36ffed3
brajpatel <brajeshpatel0002@gmail.com>
2023-12-14 14:27:07 +0000

    Correct typo

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 
 exports.login = [
     passport.authenticate("local", {
-        sucessRedirect: "/",
+        successRedirect: "/",
         failureRedirect: "/"
     })
 ]
